dcf06b84d44573473dbc102a6a4822a48aa698bd
wangwei <wangweiqindong@126.com>
2023-06-29 14:50:00 +0800

    init

---
+++ TestToggle/FunctionModuleProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  FunctionModuleProtocol.swift
+//  TestToggle
+//
+//  Created by black2w on 2023/6/29.
+//
+
+import Foundation
+
+protocol FunctionModuleProtocol {
+    //功能
+    var identify: String {get}
+    var isToggle: Bool {get}
+}
